442d8cbc7424558bd79cbf2ad281ecfb1f66bf07
Maha Jamhour <63447965+mahajye90@users.noreply.github.com>
2020-10-19 20:03:44 +0300

    add docstrings

---
+++ workspace/models/train_classifier.py
@@ -0,0 +1,140 @@
+import sys
+import pandas as pd
+import sklearn
+import numpy as np
+from sqlalchemy import create_engine
+import re
+import pickle
+import nltk
+from nltk.tokenize import word_tokenize,sent_tokenize
+from nltk.stem import WordNetLemmatizer
+from nltk.corpus import stopwords
+from sklearn.neighbors import KNeighborsClassifier
+from sklearn.metrics import confusion_matrix
+from sklearn.metrics import classification_report
+from sklearn.model_selection import train_test_split
+from sklearn.metrics import accuracy_score, precision_score, recall_score, f1_score
+from sklearn.ensemble import RandomForestClassifier,AdaBoostClassifier
+from sklearn.feature_extraction.text import CountVectorizer, TfidfTransformer, TfidfVectorizer
+from sklearn.multioutput import MultiOutputClassifier
+from sklearn.pipeline import Pipeline, FeatureUnion
+from sklearn.metrics import precision_recall_fscore_support,accuracy_score,label_ranking_average_precision_score
+from sklearn.model_selection  import GridSearchCV
+from nltk.corpus import stopwords
+from nltk.stem.porter import PorterStemmer
+nltk.download('punkt')
+nltk.download('stopwords')
+nltk.download('wordnet')
+from sqlalchemy import create_engine
+
+
+def load_data(database_filepath):
+        """
+     Input : cleaned data.
+    
+    Returns:
+       X: Disaster messages.
+       Y:  Disaster categories 
+       category_name: list of the category names.
+    """
+    engine = create_engine('sqlite:///' + database_filepath)
+    df = pd.read_sql_table('messages', con=engine)
+    X = df['message'].values
+    Y = df.loc[:,'request':'direct_report']
+    category_names = Y.columns   
+    return X, Y, category_names
+  
+
+def tokenize(text):
+        """
+    Input : a disaster message
+    
+    Returns: Tokenize text (list of tokens).
+    """
+        
+    # normalize case and remove punctuation
+    text = re.sub(r"[^a-zA-Z0-9]", " ", text.lower())
+    
+    # tokenize text
+    tokens = word_tokenize(text)
+    
+    # lemmatize andremove stop words
+    stop_words = stopwords.words("english")
+    lemmatizer = WordNetLemmatizer()
+    clean_tokens = []
+    for tok in tokens:
+        clean_tok = lemmatizer.lemmatize(tok).lower().strip()
+        clean_tokens.append(clean_tok)
+
+    return clean_tokens
+    
+
+def build_model():
+    '''
+    Building a model,  pipeline, and  gridsearchcv
+    Output: Returns the model
+    '''
+    #Build a machine learning pipeline
+    pipeline = Pipeline([
+    ('vect', CountVectorizer(tokenizer=tokenize)),
+    ('tfidf', TfidfTransformer()),
+    ('clf', MultiOutputClassifier(RandomForestClassifier()))])
+    parameters = { 'clf__estimator__criterion': ['gini', 'entropy'],
+                #'clf__estimator__min_samples_split': [2,4],
+               # 'clf__estimator__n_estimators': [100, 150, 200],
+               # 'clf__estimator__bootstrap': [True,False]
+               }
+    cv = GridSearchCV(pipeline, param_grid=parameters)
+    return cv
+     
+
+def evaluate_model(model, X_test, Y_test, category_names):
+    '''
+    evaluate_model 
+    Inputs: Model, X_test, y_test, Catgegory_names
+    return: the classification report 
+    '''
+    # predict on test data with tuned params
+    Y_pred = model.predict(X_test)
+    print(classification_report(Y_test, Y_pred))
+
+
+def save_model(model, model_filepath):
+        '''
+    Input: model and the file path to save the model
+    Output: save the model as pickle file in the give filepath 
+    '''
+    # export model to pickle file
+    pickle.dump(model, open(model_filepath, 'wb'))
+
+
+def main():
+    if len(sys.argv) == 3:
+        database_filepath, model_filepath = sys.argv[1:]
+        print('Loading data...\n    DATABASE: {}'.format(database_filepath))
+        X, Y, category_names = load_data(database_filepath)
+        X_train, X_test, Y_train, Y_test = train_test_split(X, Y, test_size=0.2)
+        
+        print('Building model...')
+        model = build_model()
+        
+        print('Training model...')
+        model.fit(X_train, Y_train)
+        
+        print('Evaluating model...')
+        evaluate_model(model, X_test, Y_test, category_names)
+
+        print('Saving model...\n    MODEL: {}'.format(model_filepath))
+        save_model(model, model_filepath)
+
+        print('Trained model saved!')
+
+    else:
+        print('Please provide the filepath of the disaster messages database '\
+              'as the first argument and the filepath of the pickle file to '\
+              'save the model to as the second argument. \n\nExample: python '\
+              'train_classifier.py ../data/DisasterResponse.db classifier.pkl')
+
+
+if __name__ == '__main__':
+    main()
